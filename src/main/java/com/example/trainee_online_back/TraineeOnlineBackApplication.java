beb567d1bbb3e2e9c5309604fdf5e00a4ae59eaf
王洋洋 <291882490@qq.com>
2022-05-09 17:38:52 +0800

    项目初始化

---
+++ src/main/java/com/example/trainee_online_back/TraineeOnlineBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.trainee_online_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraineeOnlineBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TraineeOnlineBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/trainee_online_back/TraineeOnlineBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trainee_online_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraineeOnlineBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
