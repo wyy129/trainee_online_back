dfd7ea709fa7d4091325a0114138485dcc97a0eb
wangyangyang <291882490@qq.com>
2022-06-08 11:35:31 +0800

    添加登录控制器

---
+++ src/main/java/com/example/trainee_online_back/controller/LoignController.java
@@ -0,0 +1,17 @@
+package com.example.trainee_online_back.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wangyangyang
+ * @description: 用户登录控制器
+ * @date: 2022/6/8 11:34
+ */
+@RestController
+public class LoignController {
+    @RequestMapping("/login")
+    public void login() {
+
+    }
+}
