c005bc89d7e6a4868072ac44b6361ebb7e61dcc9
wangyangyang <291882490@qq.com>
2022-06-09 11:17:38 +0800

    增加获取路由接口（未实现，思路：可以根据role的值，去redis里面获取路由）

---
+++ src/main/java/com/example/trainee_online_back/service/impl/UserRouterServiceImpl.java
@@ -0,0 +1,18 @@
+package com.example.trainee_online_back.service.impl;
+
+import com.alibaba.fastjson.JSONObject;
+import com.example.trainee_online_back.service.UserRouterService;
+
+/**
+ * @author wangyangyang
+ * @description: 用户路由实现类
+ * @date: 2022/6/9 11:01
+ */
+public class UserRouterServiceImpl implements UserRouterService {
+
+    @Override
+    public JSONObject getRouter(Integer role) {
+
+        return null;
+    }
+}
