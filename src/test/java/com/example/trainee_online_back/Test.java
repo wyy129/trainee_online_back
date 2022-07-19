a50f6844665e1a98281aa77f361ed7d4f6a6657b
王洋洋 <291882490@qq.com>
2022-07-19 21:37:09 +0800

    整合邮件发送依赖

--- src/test/java/com/example/trainee_online_back/Test.java
+++ src/test/java/com/example/trainee_online_back/Test.java
@@ -5,11 +5,16 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.example.trainee_online_back.entity.User;
 import com.example.trainee_online_back.mapper.UserMapper;
 import com.example.trainee_online_back.utils.RedisCache;
+import org.junit.jupiter.api.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 import java.util.Date;
 import java.util.List;
 
+@SpringBootTest
 public class Test {
     @Autowired
     private UserMapper userMapper;
@@ -34,6 +39,21 @@ public class Test {
     public void test2() {
         List<User> userList = userMapper.selectList(null);
         System.out.println(userList);
+    }
+
+    @Autowired
+    JavaMailSender javaMailSender;
+    @org.junit.jupiter.api.Test
+    void contextLoads() throws Exception {
+        SimpleMailMessage message=new SimpleMailMessage();
+        //发件人
+        message.setFrom("waimouserver@foxmail.com");
+        message.setText("内容");
+        message.setSubject("主题");
+        message.setTo("291882490@qq.com");
+//        message.setCc("抄送人");
+//        message.setBcc("密送人");
+        javaMailSender.send(message);
     }
 
 }
